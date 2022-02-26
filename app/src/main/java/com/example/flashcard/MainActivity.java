d991916759f2da0a9a845acc446f3b9597bf7370
Steven Tan <szt4@scarletmail.rutgers.edu>
2022-02-26 18:53:32 -0500

    Third Commit

---
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.flashcard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView question = findViewById(R.id.flashcard_question);
+        TextView answer = findViewById(R.id.flashcard_answer);
+        question.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                question.setVisibility(View.INVISIBLE);
+                answer.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
