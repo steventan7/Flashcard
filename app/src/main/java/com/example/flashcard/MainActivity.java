eb48ae470020eab8d04a9f41754c7a89f28dfebd
Steven Tan <szt4@scarletmail.rutgers.edu>
2022-03-19 19:43:49 -0400

    Third Commit

--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -12,13 +12,27 @@ import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.List;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashcards;
+    int currentCardDisplayIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        flashcardDatabase = new FlashcardDatabase((getApplicationContext()));
+        allFlashcards = flashcardDatabase.getAllCards();
+
+        if (allFlashcards != null && allFlashcards.size() != 0) {
+            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
+        }
         TextView question = findViewById(R.id.flashcard_question);
         TextView answer = findViewById(R.id.flashcard_answer);
         question.setOnClickListener(v -> {
@@ -30,17 +44,53 @@ public class MainActivity extends AppCompatActivity {
             answer.setVisibility(View.INVISIBLE);
         });
 
-        findViewById(R.id.plus).setOnClickListener(v -> {
+        findViewById(R.id.plus).setOnClickListener(v -> {       // button that goes to addCardActivity
             Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
             MainActivity.this.startActivityForResult(intent,100);
         });
 
-        findViewById(R.id.edit).setOnClickListener(v -> {
+        findViewById(R.id.edit).setOnClickListener(v -> {       // button that allows you to edit the current card
             Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
             intent.putExtra("stringKey1", ((TextView) findViewById(R.id.flashcard_question)).getText().toString());
             intent.putExtra("stringKey2", ((TextView) findViewById(R.id.flashcard_answer)).getText().toString());
             MainActivity.this.startActivityForResult(intent,100);
         });
+
+        // button that deletes current card
+        findViewById(R.id.delete).setOnClickListener(v -> {
+            if (allFlashcards.size() == 0) {
+                ((TextView) findViewById(R.id.flashcard_question)).setText("Your list of cards is empty");
+                ((TextView) findViewById(R.id.flashcard_answer)).setText("Your list of cards is empty");
+            } else {
+                flashcardDatabase.deleteCard(((TextView) findViewById(R.id.flashcard_question)).getText().toString());
+                allFlashcards = flashcardDatabase.getAllCards();
+                currentCardDisplayIndex++;
+                if (allFlashcards.size() > 0) {
+                    ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
+                    ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
+                }
+            }
+        });
+
+        findViewById(R.id.next).setOnClickListener(v -> {           // button that goes to next card
+            if (allFlashcards.size() == 0) {
+                return;
+            }
+            currentCardDisplayIndex++;
+            if (currentCardDisplayIndex >= allFlashcards.size()) {
+                Snackbar.make(findViewById(R.id.flashcard_question),
+                        "You've reached the end of the cards, going back to start.",
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+                currentCardDisplayIndex = 0;
+            }
+            allFlashcards = flashcardDatabase.getAllCards();
+            Flashcard flashcard = allFlashcards.get(currentCardDisplayIndex);
+
+            ((TextView) findViewById(R.id.flashcard_question)).setText(flashcard.getAnswer());
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(flashcard.getQuestion());
+        });
+
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -56,6 +106,10 @@ public class MainActivity extends AppCompatActivity {
                     "Card has been successfully created!",
                     Snackbar.LENGTH_SHORT)
                     .show();
+            flashcardDatabase.insertCard(new Flashcard(q,a));
+            allFlashcards = flashcardDatabase.getAllCards();
         }
+
     }
+
 }
